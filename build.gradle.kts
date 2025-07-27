e66bf066050561f3f307d6188be5a374039c0cf6
Mahmoud Khairy <mahmoud5airy2000@gmail.com>
2025-07-27 20:31:35 +0300

    add : ktlint integration add : ktlint rule in .editorConfig file

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,3 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
@@ -12,4 +11,34 @@ plugins {
     alias(libs.plugins.firebase.crashlytics) apply false
     alias(libs.plugins.firebase.performance) apply false
     alias(libs.plugins.ksp) apply false
+
+    id("org.jlleitschuh.gradle.ktlint") version "11.6.0"
+}
+subprojects {
+    pluginManager.apply("org.jlleitschuh.gradle.ktlint")
+
+    configure<org.jlleitschuh.gradle.ktlint.KtlintExtension> {
+        debug.set(true)
+        verbose.set(true)
+        android.set(true)
+
+        outputToConsole.set(true)
+
+        outputColorName.set("RED")
+        ignoreFailures.set(true)
+        enableExperimentalRules.set(false)
+
+        reporters {
+            reporter(org.jlleitschuh.gradle.ktlint.reporter.ReporterType.HTML)
+            reporter(org.jlleitschuh.gradle.ktlint.reporter.ReporterType.CHECKSTYLE)
+        }
+
+        filter {
+            exclude("**/generated/**")
+            include("**/kotlin/**")
+        }
+        dependencies {
+            ktlintRuleset("io.nlopez.compose.rules:ktlint:0.4.18")
+        }
+    }
 }
